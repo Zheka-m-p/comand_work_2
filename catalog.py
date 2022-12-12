3d3dba83b2223b6941554cb230f49ef8213f1b5c
Zheka-m-p <96652625+Zheka-m-p@users.noreply.github.com>
2022-12-12 05:21:55 +0300

    Add files via upload

---
+++ catalog.py
@@ -0,0 +1,61 @@
+class Product:
+    id: int
+    title: str
+    price: float
+    count: int
+    category: int
+
+    def __init__(self, id, title, price, count, category):
+        self.id = id
+        self.title = title
+        self.price = price
+        self.count = count
+        self.category = category
+
+    def __bool__(self):
+        """
+        Проверяет есть ли товар в наличии
+        """
+        return bool(self.count > 0)
+
+    def __len__(self):
+        """
+        Возвращает количество товара на складе
+        """
+        return self.count
+
+
+class Category:
+    id: int
+    title: str
+    description: str
+    products: list
+
+    def __init__(self, id, title, description, products):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.products = products
+
+    def __bool__(self):
+        """
+        Проверяет есть ли товары в категории
+        """
+        return bool(len(self.products))
+        # return bool(any([True for x in self.products if x.title in x])) # есть ли конкретный товар в категории
+
+    def __len__(self):
+        """
+        Возвращает количество наименований товаров, у которых есть наличие на складе
+        """
+        return sum([1 for product in self.products if product.count > 0])
+
+
+p1 = Product(1, 'апельсин', 34.5, 15, 1)
+p2 = Product(2, 'мандарин', 47.5, 26, 1)
+p3 = Product(2, 'банан', 25.3, 0, 1)
+assert bool(p3) is False  # товара р3 нет в наличии на складе
+assert len(p2) == 26  # товара p2 на складе 26 штук
+P = Category(1, 'фрукты', 'вкусные', [p1, p2, p3])
+assert bool(P) is True  # есть товары в данной категории
+assert len(P) == 2  # количество различных товаров, из данной категории, которые есть на складе в данный момент
---
+++ connector.py
@@ -0,0 +1,103 @@
+# import  json
+import json
+
+
+class Connector:
+    """
+    Класс коннектор к файлу, обязательно файл должен быть в json формате
+    не забывать проверять целостность данных, что файл с данными не подвергся
+    внешнего деградации
+    """
+    __data_file = None
+
+    def __init__(self, df):
+        self.__data_file = df
+        self.__connect()
+
+    def __connect(self):
+        """
+        Проверка на существование файла с данными и
+        создание его при необходимости
+        """
+        with open(self.__data_file, 'w'):
+            pass
+
+    def insert(self, data):
+        """
+        Запись данных в файл с сохранением структуры и исходных данных
+        """
+        with open(self.__data_file, 'r', encoding='UTF-8') as file_read:
+            try:  # если файл пустой, то избегаем ошибки
+                res = json.load(file_read)
+            except json.decoder.JSONDecodeError:  # и создаём в res пустой список
+                res = []
+        with open(self.__data_file, 'w', encoding='UTF-8') as file_write:
+            res.append(data)
+            json.dump(res, file_write, indent=4)
+
+    def select(self, query):
+        """
+        Выбор данных из файла с применением фильтрации
+        query содержит словарь, в котором ключ это поле для
+        фильтрации, а значение это искомое значение, например:
+        {'price': 1000}, должно отфильтровать данные по полю price
+        и вернуть все строки, в которых цена 1000
+        """
+        with open(self.__data_file, 'r', encoding='UTF-8') as file_for_read:
+            try:  # если файл пустой, то избегаем ошибки
+                all_data = json.load(file_for_read)
+            except json.decoder.JSONDecodeError:  # и создаём в all_data пустой список
+                all_data = []
+            if len(query) == 0:
+                return list(all_data)
+            else:
+                res = []
+                key_sort, *other_keys = query.keys()
+                value_sort, *other_values = query.values()
+                for dictionary in all_data:
+                    for k, v in dictionary.items():
+                        if k == key_sort and v == value_sort:
+                            res.append(dictionary)
+                return res
+
+    def delete(self, query):
+        """
+        Удаление записей из файла, которые соответствуют запрос,
+        как в методе select
+        """
+        with open(self.__data_file, 'r', encoding='UTF-8') as file_for_read:
+            try:  # если файл пустой, то избегаем ошибки
+                all_data = json.load(file_for_read)
+            except json.decoder.JSONDecodeError:  # и создаём в all_data пустой список
+                all_data = []
+        with open(self.__data_file, 'w', encoding='UTF-8') as file_for_write:
+            if len(query) == 0:
+                json.dump([], file_for_write, indent=4)
+                # json.dump(all_data, file_for_write, indent=4)  # или надо было вернуть все данные, если пустой словарь
+            else:
+                res = []
+                key_sort, *other_keys = query.keys()
+                value_sort, *other_values = query.values()
+                for dictionary in all_data:
+                    if key_sort not in dictionary or dictionary[key_sort] != value_sort:
+                        res.append(dictionary)
+                json.dump(res, file_for_write, indent=4)
+
+
+if __name__ == '__main__':
+    df = Connector('df.json')
+
+    data_for_file = {'id': 1, 'title': 'tet'}
+
+    df.insert(data_for_file)
+
+    # data_for_file = {'id': 2, 'title': 'fsfa'} #
+    #
+    # df.insert(data_for_file)
+
+    data_from_file = df.select({'id': 1})
+    assert data_from_file == [data_for_file]
+
+    df.delete(dict())
+    data_from_file = df.select(dict())
+    assert data_from_file == []
