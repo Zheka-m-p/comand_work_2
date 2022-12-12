37c65540ef7115e1ce653d94faefcf59b95eb501
Zheka-m-p <96652625+Zheka-m-p@users.noreply.github.com>
2022-12-13 01:24:12 +0300

    Update catalog.py

--- catalog.py
+++ catalog.py
@@ -1,3 +1,6 @@
+from connector2 import Connector
+
+
 class Product:
     id: int
     title: str
@@ -18,6 +21,12 @@ class Product:
         """
         return bool(self.count > 0)
 
+    def __str__(self):
+        return f'Продукт: {self.title}. Осталось {self.count} штук. Цена: {self.price} за кг.'
+
+    def __repr__(self):
+        return f'Продукт: {self.title}. Осталось {self.count} штук. Цена: {self.price} за кг.'
+
     def __len__(self):
         """
         Возвращает количество товара на складе
@@ -31,7 +40,7 @@ class Category:
     description: str
     products: list
 
-    def __init__(self, id, title, description, products):
+    def __init__(self, id, title, description, products=[]):
         self.id = id
         self.title = title
         self.description = description
@@ -51,12 +60,87 @@ class Category:
         return sum([1 for product in self.products if product.count > 0])
 
 
+class Shop:
+    """
+    Класс для работы с магазином
+    """
+
+    # products: list
+    # categories: list
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def get_categories(self):
+        """
+        Показать все категории пользователю в произвольном виде, главное, чтобы пользователь
+        мог видеть идентификаторы (id) каждой категории
+        """
+        our_category_connector = Connector('categories.json')
+        all_category = our_category_connector.select(dict())
+        for category in all_category:
+            print(f'Номер категории: {category["id"]}')
+            print(f'Название категории: {category["title"]}')
+            print(f'Описание категории: {category["description"]}', end='\n\n')
+
+    def get_products(self):
+        """
+        Запросить номер категории и вывести все товары, которые относятся к этой категории
+        Обработать вариант отсутствия введенного номера
+        """
+        try:
+            id_category = int(input('Введит номер категории о товарах которой вы хотите узнать: '))
+            our_category_connector = Connector('products.json')
+            all_products = our_category_connector.select({"category": (id_category)})
+            if all_products:
+                for product_ in all_products:
+                    print(f'Продукт: {product_["title"]}.'
+                          f' Осталось {product_["count"]} штук.'
+                          f' Цена: {product_["price"]} р. за кг.')
+            else:
+                print('Такой категории у нас нет!')
+        except ValueError:
+            print('Такой категории у нас нет!')
+        print()
+
+    def get_product(self):
+        """
+        Запросить ввод номера товара и вывести всю информацию по нему в произвольном виде
+        Обработать вариант отсутствия введенного номера
+        """
+        id_product = input('Введите номер товара по которому хотите узнать информацию: ')
+        our_product_connector = Connector('products.json')
+        try:  # если номер не в нашем диапазоне или не число, то выводим что номера нет такого, иначе отчёт по товару
+            product_ = our_product_connector.select({"id": int(id_product)})[0]
+            print(
+                f'Продукт: {product_["title"]}. Осталось {product_["count"]} штук. Цена: {product_["price"]} р. за кг.')
+        except IndexError:
+            print('Товара с таким номером нет!')
+        except ValueError:
+            print('Товара с таким номером нет!')
+        print()  # Просто чтобы не "слиплось"
+
+
+# if __name__ == '__main__':
+#     p1 = Product(1, 'апельсин', 34.5, 15, 1)
+#     p2 = Product(2, 'мандарин', 47.5, 26, 1)
+#     p3 = Product(2, 'банан', 25.3, 0, 1)
+#     assert bool(p3) is False  # товара р3 нет в наличии на складе
+#     assert len(p2) == 26  # товара p2 на складе 26 штук
+#     P = Category(1, 'фрукты', 'вкусные', [p1, p2, p3])
+#     assert bool(P) is True  # есть товары в данной категории
+#     assert len(P) == 2  # количество различных товаров, из данной категории, которые есть на складе в данный момент
+# if __name__ == '__main__':
+#     p1 = Product(1, 'апельсин', 34.5, 15, 1)
+#     p2 = Product(2, 'мандарин', 47.5, 26, 1)
+#     p3 = Product(2, 'банан', 25.3, 0, 1)
+#     P = Category(1, 'фрукты', 'вкусные')
+#     P.products.append(p1)
+#     P.products.append(p2)
+#     P.products.append(p3)
+#     print(P.products[1].title)
 if __name__ == '__main__':
-    p1 = Product(1, 'апельсин', 34.5, 15, 1)
-    p2 = Product(2, 'мандарин', 47.5, 26, 1)
-    p3 = Product(2, 'банан', 25.3, 0, 1)
-    assert bool(p3) is False  # товара р3 нет в наличии на складе
-    assert len(p2) == 26  # товара p2 на складе 26 штук
-    P = Category(1, 'фрукты', 'вкусные', [p1, p2, p3])
-    assert bool(P) is True  # есть товары в данной категории
-    assert len(P) == 2  # количество различных товаров, из данной категории, которые есть на складе в данный момент
+    my_shop = Shop()
+    my_shop.get_product()
+    my_shop.get_categories()
+    my_shop.get_products()
